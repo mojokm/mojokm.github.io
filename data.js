d14a91d74a0ee76dfe99ee61dcd93ca33a016aab
mojokm <hub@mojokm.com>
2024-08-07 16:13:42 +0800

    Menu

--- data.js
+++ data.js
@@ -3,10 +3,10 @@ const menuData = [
         "name": "夢雲瀚",
         "flavor": "水蜜桃/ 鳳梨/ 覆盆莓",
         "alcohol_content": "≈12% ABV",
-        "alcohol_units": 5,
+        "alcohol_units": 0,
         "price": 360,
-        "image": "調酒圖.png",
-        "icon": "高粱酒icon.png",
+        "image": "https://drive.google.com/file/d/1-LqPcZx3yY-ecqHbj00C9-A-2KejVxJc/view?usp=drive_link",
+        "icon": "https://drive.google.com/file/d/12TQmzPLCm7Fpwybi33FVdNRDFDuM4bCp/view?usp=drive_link",
         "flavor_profile": {
             "酸": 4,
             "甜": 4,
@@ -15,5 +15,68 @@ const menuData = [
             "風味強度": 2
         }
     },
-    // Add other items here...
+    {
+        "name": "夢幻水蜜桃",
+        "flavor": "水蜜桃",
+        "alcohol_content": "8% ABV",
+        "alcohol_units": 3,
+        "price": 250,
+        "image": "https://drive.google.com/file/d/1Lz8dhFSx2T/view?usp=drive_link",
+        "icon": "https://drive.google.com/file/d/1Kz8QYdA32T/view?usp=drive_link",
+        "flavor_profile": {
+            "酸": 3,
+            "甜": 5,
+            "苦": 0,
+            "酒感": 2,
+            "風味強度": 4
+        }
+    },
+    {
+        "name": "鳳梨爽",
+        "flavor": "鳳梨",
+        "alcohol_content": "10% ABV",
+        "alcohol_units": 4,
+        "price": 280,
+        "image": "https://drive.google.com/file/d/1Zz8dHSs/view?usp=drive_link",
+        "icon": "https://drive.google.com/file/d/1Qz8WYaT/view?usp=drive_link",
+        "flavor_profile": {
+            "酸": 2,
+            "甜": 4,
+            "苦": 1,
+            "酒感": 3,
+            "風味強度": 3
+        }
+    },
+    {
+        "name": "覆盆莓之夢",
+        "flavor": "覆盆莓",
+        "alcohol_content": "9% ABV",
+        "alcohol_units": 3,
+        "price": 270,
+        "image": "https://drive.google.com/file/d/1Xz8eWS/view?usp=drive_link",
+        "icon": "https://drive.google.com/file/d/1Lz8UYT/view?usp=drive_link",
+        "flavor_profile": {
+            "酸": 5,
+            "甜": 3,
+            "苦": 0,
+            "酒感": 2,
+            "風味強度": 4
+        }
+    },
+    {
+        "name": "檸檬清新",
+        "flavor": "檸檬",
+        "alcohol_content": "7% ABV",
+        "alcohol_units": 2,
+        "price": 240,
+        "image": "https://drive.google.com/file/d/1Wz8HZv/view?usp=drive_link",
+        "icon": "https://drive.google.com/file/d/1Pz8XvT/view?usp=drive_link",
+        "flavor_profile": {
+            "酸": 4,
+            "甜": 2,
+            "苦": 1,
+            "酒感": 1,
+            "風味強度": 3
+        }
+    }
 ];
--- script.js
+++ script.js
@@ -14,9 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 <span>酒精濃度: ${item.alcohol_content}</span>
                 <div class="alcohol-units">${getAlcoholUnits(item.alcohol_units)}</div>
             </div>
+            <canvas class="radar-chart" id="radar-chart-${item.name.replace(/ /g, '-')}"></canvas>
         `;
 
         menuContainer.appendChild(menuItem);
+
+        // Generate radar chart
+        generateRadarChart(`radar-chart-${item.name.replace(/ /g, '-')}`, item.flavor_profile);
     });
 });
 
@@ -26,4 +30,28 @@ function getAlcoholUnits(units) {
         unitIcons += `<img src="path_to_your_icon.png" alt="unit">`;
     }
     return unitIcons;
+}
+
+function generateRadarChart(containerId, flavorProfile) {
+    const data = {
+        labels: ['酸', '甜', '苦', '酒感', '風味強度'],
+        datasets: [{
+            label: 'Flavor Profile',
+            data: [flavorProfile.酸, flavorProfile.甜, flavorProfile.苦, flavorProfile.酒感, flavorProfile.風味強度],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    };
+
+    const ctx = document.getElementById(containerId).getContext('2d');
+    new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: {
+            scale: {
+                ticks: { beginAtZero: true }
+            }
+        }
+    });
 }
