c1f2b0ab525bb45fc71afad220394188ae3a9e53
mojokm <hub@mojokm.com>
2024-08-07 15:57:59 +0800

    .

---
+++ data.js
@@ -0,0 +1,19 @@
+const menuData = [
+    {
+        "name": "夢雲瀚",
+        "flavor": "水蜜桃/ 鳳梨/ 覆盆莓",
+        "alcohol_content": "≈12% ABV",
+        "alcohol_units": 5,
+        "price": 360,
+        "image": "調酒圖.png",
+        "icon": "高粱酒icon.png",
+        "flavor_profile": {
+            "酸": 4,
+            "甜": 4,
+            "苦": 0,
+            "酒感": 1,
+            "風味強度": 2
+        }
+    },
+    // Add other items here...
+];
